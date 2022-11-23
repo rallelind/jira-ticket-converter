74d6bc206c4ee62ae62ab24f7dd77aac12276268
Rasmus Lind <rasmus@voiceboxer.com>
2022-11-23 09:53:06 +0100

    added labels formatting

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const ObjectsToCsv = require('objects-to-csv');
 const path = require('path');
 const fs = require('fs');
 
-const FILE = '/example/example.xlsx'
+const FILE = '/Users/rasmuslind/Desktop/excel-to-jira/report.xlsx'
 
 let formatTicket = (row, heading) => {
     let ticketData = {}
 
-    let description = `${heading[2]}\n${row[2]}\n${heading[3]}\n${row[3]}\n${heading[4]}\n${row[4]}\n${heading[5]}\n${row[5]}\n${heading[7]}\n${row[7]}\n${heading[8]}\n${row[8]}\n${heading[9]}\n${row[9]}\n${heading[10]}\n${row[10]}\n${heading[11]}\n${row[11]}\n${heading[12]}\n${row[12]}\n${heading[13]}\n${row[13]}`
+    let description = `h3. ${heading[2]}\n${row[2]}\n\nh3. ${heading[3]}\n${row[3]}\n\nh3. ${heading[4]}\n${row[4]}\n\nh3. ${heading[5]}\n${row[5]}\n\nh3. ${heading[7]}\n${row[7]}\n\nh3. ${heading[8]}\n${row[8]}\n\nh3. ${heading[9]}\n${row[9]}\n\nh3. ${heading[10]}\n${row[10]}\n\nh3. ${heading[11]}\n${row[11]}\n\nh3. ${heading[12]}\n${row[12]}\n\nh3. ${heading[13]}\n${row[13]}`
 
     ticketData.priority = row[1]
+    ticketData.labels = row[5]
     ticketData.issueType = "bug"
     ticketData.summary = row[6]
     ticketData.description = description
@@ -55,4 +56,4 @@ const convertToTicketFromFile = (file, sheet = 1) => {
     })
 }
 
-convertToTicketFromFile(FILE)
+convertToTicketFromFile(FILE, "Color Contrast")
