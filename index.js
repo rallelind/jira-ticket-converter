2d27c62b342d0474fafa364feffde65962f66ab8
Rasmus Lind <rasmus@voiceboxer.com>
2022-11-23 13:29:34 +0100

    added comments to ticketdata

--- index.js
+++ index.js
@@ -8,15 +8,14 @@ const FILE = '/Users/rasmuslind/Desktop/report.xlsx'
 let formatTicket = (row, heading, linkToIssue = true) => {
     let ticketData = {}
 
-    ticketData.priority = row[1]
-    ticketData.labels = row[5]
+    ticketData.priority = row[1] // Use Serverity column for priority in Jira ticket
+    ticketData.labels = row[5] // Use Page column for label in Jira ticket
     ticketData.issueType = "bug"
-    ticketData.summary = row[6]
+    ticketData.summary = row[6] // Use Summary column for summary in Jira ticket
 
     // Very specific to BoostEvents as we wanted to link to these tickets to other issues. 
     // To link to any other issue create another issue in jira and set the jira name in ticket.blocks to link the two
     // To disable, just change function parameter linkToIssue to false and then the ticket will not contain a link to other tickets
-
     if(linkToIssue) {
         if(row[7].includes("portal.voiceboxer")) {
             ticketData.blocks = "VPAT-824"
@@ -26,7 +25,9 @@ let formatTicket = (row, heading, linkToIssue = true) => {
         }
     }
 
+    // Format a string for description in Jira from the other rows in the excel report
     let description = `h3. ${heading[2]}\n${row[2]}\n\nh3. ${heading[3]}\n${row[3]}\n\nh3. ${heading[4]}\n${row[4]}\n\nh3. ${heading[5]}\n${row[5]}\n\nh3. ${heading[7]}\n${row[7]}\n\nh3. ${heading[8]}\n${row[8]}\n\nh3. ${heading[9]}\n${row[9]}\n\nh3. ${heading[10]}\n${row[10]}\n\nh3. ${heading[11]}\n${row[11]}\n\nh3. ${heading[12]}\n${row[12]}\n\nh3. ${heading[13]}\n${row[13]}`
+    
     ticketData.description = description
 
     return ticketData
@@ -69,4 +70,4 @@ const convertToTicketFromFile = (file, sheet = 1) => {
     })
 }
 
-convertToTicketFromFile(FILE, "Color Contrast")
+convertToTicketFromFile(FILE, "Axe")
